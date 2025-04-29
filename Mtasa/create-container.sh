323ac92b771d84f0d320d458c91fb8660460c76a
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-04-29 17:02:22 -0300

    fix: remover opção -d do comando docker create

--- Mtasa/create-container.sh
+++ Mtasa/create-container.sh
@@ -59,7 +59,7 @@ wget https://supremesolid.github.io/ubuntu-automation-tools/Mtasa/entrypoint.sh
 chown mtasa:mtasa entrypoint.sh
 chmod -R 777 entrypoint.sh
 
-docker create -it -d \
+docker create -it \
   --name "$NAME" \
   --network host \
   --workdir "/home/mtasa/$NAME" \
