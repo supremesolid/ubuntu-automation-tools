2928b6a4cd6ef15ea47fcf198ba511d67a0e6733
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-04-29 16:21:28 -0300

    feat: adicionar script para criação de contêiner do servidor MTA

---
+++ Mtasa/create-container.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+NAME="$1"
+
+if [ -z "$1" ]; then
+    echo "Erro: O nome não fornecido."
+    echo "Uso: $0 nome"
+    exit 1
+fi
+
+cd /home/mtasa/
+
+wget https://linux.multitheftauto.com/dl/multitheftauto_linux_x64.tar.gz
+
+tar xfs multitheftauto_linux_x64.tar.gz
+
+rm -rf multitheftauto_linux_x64.tar.gz
+
+mv multitheftauto_linux_x64 $NAME
+
+cd $NAME/mods/deathmatch/
+
+wget https://linux.multitheftauto.com/dl/baseconfig.tar.gz
+
+tar xfs baseconfig.tar.gz
+
+cd baseconfig
+
+mv * ../
+
+cd ../
+
+rm -rf baseconfig.tar.gz baseconfig
+
+mkdir resources
+
+cd resources
+
+wget https://mirror-cdn.multitheftauto.com/mtasa/resources/mtasa-resources-latest.zip
+
+unzip mtasa-resources-latest.zip
+
+rm -rf mtasa-resources-latest.zip
+
+cd /home/mtasa
+
+chown -R mtasa:mtasa $NAME
+
+
