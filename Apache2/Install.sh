9bb13cbe0255fe2d58d63d30c90556b8f19fad97
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-05-04 15:16:19 -0300

    feat: adicionar script de instalação do Apache2 com configuração do mpm_itk

---
+++ Apache2/Install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+apt-get install -y apache2 libapache2-mpm-itk
+
+mpm_itk="/etc/apache2/mods-available/mpm_itk.conf"
+
+echo '<IfModule mpm_itk.c>' > "$mpm_itk"
+echo '    LimitUIDRange 0 4294496296' >> "$mpm_itk"
+echo '    LimitGIDRange 0 4294496296' >> "$mpm_itk"
+echo '</IfModule>' >> "$mpm_itk"
+
+a2dismod mpm_itk
+a2enmod mpm_itk
+
+ports="/etc/apache2/ports.conf"
+
+echo '<Listen 127.0.0.1:80' > "$ports"
+echo '' >> "$ports"
+echo '<IfModule ssl_module>' >> "$ports"
+echo '	Listen 127.0.0.1:443' >> "$ports"
+echo '</IfModule>' >> "$ports"
+echo '' >> "$ports"
+echo '<IfModule mod_gnutls.c>' >> "$ports"
+echo '	Listen 127.0.0.1:443' >> "$ports"
+echo '</IfModule>' >> "$ports"
+
+systemctl restart apache2
