e418041e86663b4c6c05fe5f431985e8b5919b40
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-05-04 16:28:01 -0300

    feat: atualizar script de instalação do Apache2 para corrigir configuração do mpm_itk e definir portas

--- Apache2/install.sh
+++ Apache2/install.sh
@@ -4,26 +4,28 @@ set -euo pipefail
 
 apt-get install -y apache2 libapache2-mpm-itk
 
-mpm_itk="/etc/apache2/mods-available/mpm_itk.conf"
+systemctl restart stop
 
-echo '<IfModule mpm_itk.c>' > "$mpm_itk"
-echo '    LimitUIDRange 0 4294496296' >> "$mpm_itk"
-echo '    LimitGIDRange 0 4294496296' >> "$mpm_itk"
-echo '</IfModule>' >> "$mpm_itk"
+conf_mpm_itk="/etc/apache2/mods-available/mpm_itk.conf"
+conf_ports="/etc/apache2/ports.conf"
+conf_vhost_default="/etc/apache2/sites-available/000-default.conf"
 
 a2dismod mpm_itk
+
+echo '<IfModule mpm_itk.c>' > "$conf_mpm_itk"
+echo '    LimitUIDRange 0 4294496296' >> "$conf_mpm_itk"
+echo '    LimitGIDRange 0 4294496296' >> "$conf_mpm_itk"
+echo '</IfModule>' >> "$conf_mpm_itk"
+
+echo 'Listen 127.0.0.1:8080' > "$conf_ports"
+
+echo '<VirtualHost *:8080>' > "$conf_vhost_default"
+echo '	ServerAdmin webmaster@localhost' >> "$conf_vhost_default"
+echo '	DocumentRoot /var/www/html' >> "$conf_vhost_default"
+echo '	ErrorLog ${APACHE_LOG_DIR}/error.log' >> "$conf_vhost_default"
+echo '	CustomLog ${APACHE_LOG_DIR}/access.log combined' >> "$conf_vhost_default"
+echo '</VirtualHost>' >> "$conf_vhost_default"
+
 a2enmod mpm_itk headers
-
-ports="/etc/apache2/ports.conf"
-
-echo 'Listen 127.0.0.1:80' > "$ports"
-echo '' >> "$ports"
-echo '<IfModule ssl_module>' >> "$ports"
-echo '	Listen 127.0.0.1:443' >> "$ports"
-echo '</IfModule>' >> "$ports"
-echo '' >> "$ports"
-echo '<IfModule mod_gnutls.c>' >> "$ports"
-echo '	Listen 127.0.0.1:443' >> "$ports"
-echo '</IfModule>' >> "$ports"
 
 systemctl restart apache2
