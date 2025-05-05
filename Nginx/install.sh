fee1d134c5edb1090b324b795054110afdecf0f0
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-05-05 13:07:47 -0300

    feat: adicionar configuração global do Nginx com suporte a gzip e SSL

--- Nginx/install.sh
+++ Nginx/install.sh
@@ -93,7 +93,56 @@ apt-get update -y || error_exit "Falha ao executar apt update."
 echo ">>> Instalando Nginx (apt install nginx)..."
 apt-get install -y nginx || error_exit "Falha ao instalar o Nginx."
 
+CONFIG_GLOBAL="${NGINX_CONFIG_DIR}/nginx.conf"
+
+echo ">>> Criando arquivo de configuração: ${CONFIG_GLOBAL}"
+
+bash -c "cat > ${CONFIG_GLOBAL}" <<EOF
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+error_log /var/log/nginx/error.log;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+	worker_connections 768;
+	# multi_accept on;
+}
+
+http {
+	sendfile on;
+	tcp_nopush on;
+	types_hash_max_size 2048;
+	# server_tokens off;
+
+	server_names_hash_bucket_size 128;
+	# server_name_in_redirect off;
+
+	include /etc/nginx/mime.types;
+	default_type application/octet-stream;
+
+
+	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+	ssl_prefer_server_ciphers on;
+
+	access_log /var/log/nginx/access.log;
+
+	gzip on;
+
+	# gzip_vary on;
+	# gzip_proxied any;
+	# gzip_comp_level 6;
+	# gzip_buffers 16 8k;
+	# gzip_http_version 1.1;
+	# gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+	include /etc/nginx/conf.d/*.conf;
+	include /etc/nginx/sites-enabled/*;
+}
+EOF
+
 CONFIG_FILE_PATH="${NGINX_SITES_AVAILABLE}/${DEFAULT_VHOST_NAME}"
+
 echo ">>> Criando arquivo de configuração: ${CONFIG_FILE_PATH}"
 
 bash -c "cat > ${CONFIG_FILE_PATH}" <<EOF
